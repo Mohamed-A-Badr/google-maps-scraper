a4a7da7928f3e933cb4bc15f9a9c9204032dacaf
Mohamed-A-Badr <badr49960@gmail.com>
2025-06-26 13:40:41 +0300

    Initialize Scraper class with Edge WebDriver setup and basic URL opening method

---
+++ main.py
@@ -0,0 +1,83 @@
+import time
+from urllib.parse import quote_plus
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class Scraper:
+    def __init__(self):
+        """
+        Initializes the Scraper class and sets up the Edge WebDriver.
+        """
+        self.driver = None
+        self.setup_driver()
+
+    def setup_driver(self):
+        """
+        Initializes the Edge WebDriver with specific options.
+        Sets the driver to start maximized, disables extensions, and sets the log level.
+        """
+        options = webdriver.EdgeOptions()
+        options.add_argument("--start-maximized")
+        options.add_argument("--log-level=3")
+        options.add_argument("--disable-extensions")
+
+        service = Service(log_path="msedgedriver.log")
+
+        self.driver = webdriver.Edge(options=options, service=service)
+
+    def open_url(self, url):
+        """
+        Opens the specified URL in the Edge browser.
+        Args:
+            url (str): URL to open in the browser.
+        """
+        if self.driver is not None:
+            self.driver.get(url)
+        else:
+            print("Driver not initialized.")
+
+    def loading_search_results(self, search_query, lat, long):
+        """
+        Loads search results for a given query and coordinates in Google Maps.
+        Args:
+            search_query (str): search keywords for searching
+            lat (float): latitude for the search location
+            long (float): longitude for the search location
+        """
+        encoded_query = quote_plus(search_query)
+        url = f"https://www.google.com/maps/search/{encoded_query}/@{lat},{long},16z"
+
+        self.open_url(url)
+
+        try:
+            zoom_out_button = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.ID, "widget-zoom-out"))
+            )
+        except Exception as e:
+            print(f"Error finding zoom out button: {e}")
+            return
+        zoom_out_button.click()
+        print("Zoom out button clicked.")
+
+        try:
+            search_area_button = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//button[@jsaction='search.refresh']")
+                )
+            )
+        except Exception as e:
+            print(f"Error finding search area button: {e}")
+            return
+        search_area_button.click()
+        print("Search area button clicked.")
+
+
+run_scraper = Scraper()
+run_scraper.loading_search_results(
+    "مركز أشعة OR أشعة OR سينية OR radiology", 29.9827167, 31.23571
+)
